85ab9d95fa1d2e7e2d04e262bf84ceee962ea881
S Yang <syang@ss-macbook-air.attlocal.net>
2017-12-14 23:43:52 -0800

    search lambda worked. TODO:1)passing endpoint to js; 2)clean up the documents[0] to documents

--- search-added/es_trigger.js
+++ search-added/es_trigger.js
@@ -1,40 +1,70 @@
 var AWS = require('aws-sdk');
+var path = require('path');
 
 const qaSearchDomain = process.env.ES_DOMAIN;
-
+let esDomain = {
+    region: 'us-west-1',
+    endpoint: 'search-backend-dev-searchdomain-jku36sqvljiz67awyicmg42ry4.us-west-1.es.amazonaws.com',
+    index: 'q_a',
+    doctype: 'questions'
+};
+var endpoint = new AWS.Endpoint(esDomain.endpoint);
 
 exports.handler = function(event, context) {
-    console.log("indexing is triggered: ");
-    /*
-    var es_endpoint = new AWS.Endpoint(qaSearchDomain);
+    console.log("indexing is triggered", event.Records);
+    console.log("esDomain: ", esDomain);
+
+    // var es_endpoint = new AWS.Endpoint(qaSearchDomain);
+    // assemble the document
     var documents = event.Records.map(function(record) {
-        var data = {id : record.dynamodb.Keys.id.S};
-        
+        var data = {id : record.dynamodb.Keys.category.S + 
+                         "-" + record.dynamodb.Keys.ts.S};      
         if (record.eventName === 'REMOVE') {
             data.type = 'delete'
         } else {
             var qa_entry = record.dynamodb.NewImage;
-            
             data.type = 'add'
             data.fields = {
-                name : image.name.S,
-                username : image.username.S,
-                email : image.email.S
+                question : qa_entry.question.S,
             };
+            if (qa_entry.details != null) {
+                data.fields.details = qa_entry.details.S
+            }
         }
-        
+        console.log("generated data: ", data);
         return data;
     });
-    
-    var params = {contentType: 'application/json', documents : JSON.stringify(documents) };
-    console.log('uploading documents to cloudsearch domain', params);
-    cloudsearchdomain.uploadDocuments(params, function(err, data) {
-        if(err) {
-            console.log('Error uploading documents to cloudsearch', err, err.stack);
-            context.fail(err);
-        } else {
-            context.succeed("Successfully processed " + event.Records.length + " records.");  
-        }
-    });
-    */
+    console.log("document.fields = ", documents[0].fields);
+    postToES(documents, context);
 };
+
+function postToES(documents, context) {
+    doc = documents[0];
+    var req = new AWS.HttpRequest(endpoint);
+    req.method = 'PUT';
+    req.path = path.join('/', esDomain.index, esDomain.doctype, doc.id);
+    req.region = esDomain.region;
+    // req.headers['presigned-expires'] = false;
+    req.headers['Host'] = endpoint.host;
+    req.body = JSON.stringify(doc.fields);
+    console.log("request = ", req);
+    console.log("request.body", req.body);
+
+    // var signer = new AWS.Signers.V4(req , 'es');  // es: service code
+    // signer.addAuthorization(creds, new Date());
+
+    var send = new AWS.NodeHttpClient();
+    send.handleRequest(req, null, function(httpResp) {
+        var respBody = '';
+        httpResp.on('data', function (chunk) {
+            respBody += chunk;
+        });
+        httpResp.on('end', function (chunk) {
+            console.log('Response: ' + respBody);
+            context.succeed('Lambda added document ' + doc);
+        });
+    }, function(err) {
+        console.log('Error: ' + err);
+        context.fail('Lambda failed with error ' + err);
+    });
+}
